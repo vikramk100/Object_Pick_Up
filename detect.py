e8dca0d130b49fcf69b63e05f8c1549d39c3e29d
vikramk100 <vikramr.kavalipati@gmail.com>
2025-04-12 16:05:40 -0400

    Position Detection

--- detect.py
+++ detect.py
@@ -1,13 +1,30 @@
-
+from ultralytics import YOLO
 import numpy as np
 import cv2
 
+model = YOLO('yolov8n.pt')
 cap = cv2.VideoCapture(1)
 
 while True:
     ret, frame = cap.read()
-    cv2.imshow('frame', frame)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
+
+    results = model.track(frame, persist = True)
+    results = results[0]
+    frame_p = results.plot()
+
+    for box in results.boxes:
+      coord = box.xywhn[0].tolist()
+      clss = box.cls[0].item()
+      conf = box.conf[0].item()
+
+      print(f"Class: {model.names[clss]}, Confidence: {conf:.2f}")
+      print(f"Coordinates: center_x_norm={coord[0]:.5f}, center_y_norm={coord[1]:.5f}, "
+            f"width_norm={coord[2]:.5f}, height_norm={coord[3]:.05}")
+      print("-" * 50)
+
+
+    cv2.imshow('frame', frame_p)
+    if cv2.waitKey(25) & 0xFF == ord('q'):
       break
 
 cap.release()
