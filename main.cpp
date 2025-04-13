090ec92975efb5a55bf6542d0cb46abf6a4d4605
vikramk100 <122198352+vikramk100@users.noreply.github.com>
2025-04-13 06:11:12 -0400

    Update to take actions

--- main.cpp
+++ main.cpp
@@ -18,18 +18,21 @@ void loop() {
     int comma1 = data.indexOf(',');
     int comma2 = data.indexOf(',', comma1+1);
     int comma3 = data.indexOf(',', comma2+1);
+    int comma4 = data.indexOf(',', comma3+1);
     
-    if (comma1 > 0 && comma2 > 0 && comma3 > 0) {
-      float center_x = data.substring(0, comma1).toFloat();
-      float center_y = data.substring(comma1+1, comma2).toFloat();
-      float width = data.substring(comma2+1, comma3).toFloat();
-      float height = data.substring(comma3+1).toFloat();
+    if (comma1 > 0 && comma2 > 0 && comma3 > 0 && comma4 > 0) {
+      int action = data.substring(0, comma1).toInt();
+      float center_x = data.substring(comma1+1, comma2).toFloat();
+      float center_y = data.substring(comma2+1, comma3).toFloat();
+      float width = data.substring(comma3+1, comma4).toFloat();
+      float height = data.substring(comma4+1).toFloat();
 
       digitalWrite(13, HIGH);
       delay(1000);
       digitalWrite(13, LOW);
       
       Serial.print("Received: ");
+      Serial.print(action);
       Serial.print(center_x, 5);
       Serial.print(", ");
       Serial.print(center_y, 5);
